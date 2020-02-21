e97cabf8a75621c22999a86476868529a65645b2
naunow <naunow00@gmail.com>
2020-02-22 01:11:04 +0900

    最初のコミット

---
+++ MultiCalculator/MultiCalculator/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MultiCalculator.Models;
+
+namespace MultiCalculator.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
